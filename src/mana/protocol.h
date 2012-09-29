32c867a1a535bcc735920c4b5827255f2074ca80
Erik Schilling <ablu.erikschilling@googlemail.com>
2012-09-29 15:59:53 +0200

    Synced protcol with server/other client

--- src/mana/protocol.h
+++ src/mana/protocol.h
@@ -24,7 +24,10 @@
 
 namespace Mana {
 
-enum { PROTOCOL_VERSION = 1 };
+enum {
+    PROTOCOL_VERSION = 1,
+    SUPPORTED_DB_VERSION = 21
+};
 
 /**
  * The type of a value in a message. Prepended before each value when the
@@ -52,7 +55,7 @@ enum ValueType {
  * Components: B byte, W word, D double word, S variable-size string
  *             C tile-based coordinates (B*3)
  *
- * Hosts:      P (player's client), A (account server), C (char server),
+ * Hosts:      P (player's client), A (account server), C (chat server),
  *             G (game server)
  *
  * TODO - Document specific error codes for each packet
@@ -102,16 +105,16 @@ enum {
     // Game
     GPMSG_PLAYER_MAP_CHANGE        = 0x0100, // S filename, W x, W y
     GPMSG_PLAYER_SERVER_CHANGE     = 0x0101, // B*32 token, S game address, W game port
-    PGMSG_PICKUP                   = 0x0110, // W*2 position
-    PGMSG_DROP                     = 0x0111, // B slot, B amount
-    PGMSG_EQUIP                    = 0x0112, // B slot
-    PGMSG_UNEQUIP                  = 0x0113, // B slot
-    PGMSG_MOVE_ITEM                = 0x0114, // B slot1, B slot2, B amount
+    PGMSG_PICKUP                   = 0x0110, // W * 2 items position
+    PGMSG_DROP                     = 0x0111, // W slot, W amount
+    PGMSG_EQUIP                    = 0x0112, // W inventory slot
+    PGMSG_UNEQUIP                  = 0x0113, // W item Instance id
+    PGMSG_MOVE_ITEM                = 0x0114, // W slot1, W slot2, W amount
     GPMSG_INVENTORY                = 0x0120, // { W slot, W item id [, W amount] (if item id is nonzero) }*
-    GPMSG_INVENTORY_FULL           = 0x0121, // W inventory slot count { W slot, W itemId, W amount }, { B equip slot, W invy slot}*
-    GPMSG_EQUIP                    = 0x0122, // { W Invy slot, B equip slot type count { B equip slot, B number used} }*
+    GPMSG_INVENTORY_FULL           = 0x0121, // W inventory slot count { W slot, W itemId, W amount }, { W equip slot, W item Id, W item Instance}*
+    GPMSG_EQUIP                    = 0x0122, // W item Id, W equip slot type count //{ W equip slot, W capacity used}*//<- When equipping, //{ W item instance, W 0}*//<- When unequipping
     GPMSG_PLAYER_ATTRIBUTE_CHANGE  = 0x0130, // { W attribute, D base value (in 1/256ths), D modified value (in 1/256ths)}*
-    GPMSG_PLAYER_EXP_CHANGE        = 0x0140, // { W skill, D exp got, D exp needed }*
+    GPMSG_PLAYER_EXP_CHANGE        = 0x0140, // { W skill, D exp got, D exp needed, W skill level }*
     GPMSG_LEVELUP                  = 0x0150, // W new level, W character points, W correction points
     GPMSG_LEVEL_PROGRESS           = 0x0151, // B percent completed to next levelup
     PGMSG_RAISE_ATTRIBUTE          = 0x0160, // W attribute
@@ -120,24 +123,25 @@ enum {
     GPMSG_LOWER_ATTRIBUTE_RESPONSE = 0x0171, // B error, W attribute
     PGMSG_RESPAWN                  = 0x0180, // -
     GPMSG_BEING_ENTER              = 0x0200, // B type, W being id, B action, W*2 position, B direction
-                                             // character: S name, B hair style, B hair color, B gender, B item bitmask, { W item id }*
-                                             // monster: W type id
-                                             // npc: W type id
+                                             // character: S name, B hair style, B hair color, B gender, B sprite layers changed, { B slot type, W item id }*
+                                             // monster: W type id gender
+                                             // npc: W type id gender
     GPMSG_BEING_LEAVE              = 0x0201, // W being id
     GPMSG_ITEM_APPEAR              = 0x0202, // W item id, W*2 position
-    GPMSG_BEING_LOOKS_CHANGE       = 0x0210, // W weapon, W hat, W top clothes, W bottom clothes
+    GPMSG_BEING_LOOKS_CHANGE       = 0x0210, // B sprite layers changed, { B slot type, W item id }*
     PGMSG_WALK                     = 0x0260, // W*2 destination
     PGMSG_ACTION_CHANGE            = 0x0270, // B Action
     GPMSG_BEING_ACTION_CHANGE      = 0x0271, // W being id, B action
     PGMSG_DIRECTION_CHANGE         = 0x0272, // B Direction
     GPMSG_BEING_DIR_CHANGE         = 0x0273, // W being id, B direction
     GPMSG_BEING_HEALTH_CHANGE      = 0x0274, // W being id, W hp, W max hp
-    GPMSG_BEINGS_MOVE              = 0x0280, // { W being id, B flags [, W*2 position, B speed] }*
+    GPMSG_BEINGS_MOVE              = 0x0280, // { W being id, B flags [, [W*2 position,] W*2 destination, B speed] }*
     GPMSG_ITEMS                    = 0x0281, // { W item id, W*2 position }*
     PGMSG_ATTACK                   = 0x0290, // W being id
-    GPMSG_BEING_ATTACK             = 0x0291, // W being id, B direction, B attacktype
-    PGMSG_USE_SPECIAL              = 0x0292, // B specialID
+    GPMSG_BEING_ATTACK             = 0x0291, // W being id, B direction, B attack Id
+    PGMSG_USE_SPECIAL_ON_BEING     = 0x0292, // B specialID, W being id
     GPMSG_SPECIAL_STATUS           = 0x0293, // { B specialID, D current, D max, D recharge }
+    PGMSG_USE_SPECIAL_ON_POINT     = 0x0294, // B specialID, W*2 position
     PGMSG_SAY                      = 0x02A0, // S text
     GPMSG_SAY                      = 0x02A1, // W being id, S text
     GPMSG_NPC_CHOICE               = 0x02B0, // W being id, { S text }*
@@ -177,13 +181,15 @@ enum {
     GPMSG_BEINGS_DAMAGE            = 0x0310, // { W being id, W amount }*
     GPMSG_CREATE_EFFECT_POS        = 0x0320, // W effect id, W*2 position
     GPMSG_CREATE_EFFECT_BEING      = 0x0321, // W effect id, W BeingID
+    GPMSG_CREATE_TEXT_PARTICLE     = 0x0322, // S text
+    GPMSG_SHAKE                    = 0x0330, // W intensityX, W intensityY, [W decay_times_10000, [W duration]]
 
     // Guild
     PCMSG_GUILD_CREATE                  = 0x0350, // S name
     CPMSG_GUILD_CREATE_RESPONSE         = 0x0351, // B error, W guild, B rights, W channel
     PCMSG_GUILD_INVITE                  = 0x0352, // W id, S name
     CPMSG_GUILD_INVITE_RESPONSE         = 0x0353, // B error
-    PCMSG_GUILD_ACCEPT                  = 0x0354, // W id
+    PCMSG_GUILD_ACCEPT                  = 0x0354, // W id, B accepted (0 if false, 1 if true)
     CPMSG_GUILD_ACCEPT_RESPONSE         = 0x0355, // B error, W guild, B rights, W channel
     PCMSG_GUILD_GET_MEMBERS             = 0x0356, // W id
     CPMSG_GUILD_GET_MEMBERS_RESPONSE    = 0x0357, // S names, B online
@@ -194,30 +200,30 @@ enum {
     CPMSG_GUILD_PROMOTE_MEMBER_RESPONSE = 0x0366, // B error
     PCMSG_GUILD_KICK_MEMBER             = 0x0370, // W guild, S name
     CPMSG_GUILD_KICK_MEMBER_RESPONSE    = 0x0371, // B error
+    CPMSG_GUILD_KICK_NOTIFICATION       = 0x0372, // W guild, S player that kicked
 
     CPMSG_GUILD_INVITED                 = 0x0388, // S char name, S  guild name, W id
     CPMSG_GUILD_REJOIN                  = 0x0389, // S name, W guild, W rights, W channel, S announce
 
     // Party
-    PCMSG_PARTY_INVITE                  = 0x03A0, // S name
-    CPMSG_PARTY_INVITE_RESPONSE         = 0x03A1, // B error, S name
-    CPMSG_PARTY_INVITED                 = 0x03A2, // S name
-    PCMSG_PARTY_ACCEPT_INVITE           = 0x03A5, // S name
-    CPMSG_PARTY_ACCEPT_INVITE_RESPONSE  = 0x03A6, // B error, { S name }
-    PCMSG_PARTY_REJECT_INVITE           = 0x03A7, // S name
-    CPMSG_PARTY_REJECTED                = 0x03A8, // S name
+    PGMSG_PARTY_INVITE                  = 0x03A0, // S name
+    GPMSG_PARTY_INVITE_ERROR            = 0x03A1, // S name
+    GCMSG_PARTY_INVITE                  = 0x03A2, // S inviter, S invitee
+    CPMSG_PARTY_INVITED                 = 0x03A4, // S name
+    PCMSG_PARTY_INVITE_ANSWER           = 0x03A5, // S name, B accept
+    CPMSG_PARTY_INVITE_ANSWER_RESPONSE  = 0x03A6, // B error, { S name }
+    CPMSG_PARTY_REJECTED                = 0x03A8, // S name, B error
     PCMSG_PARTY_QUIT                    = 0x03AA, // -
     CPMSG_PARTY_QUIT_RESPONSE           = 0x03AB, // B error
-    CPMSG_PARTY_NEW_MEMBER              = 0x03B0, // W being id, S name
-    CPMSG_PARTY_MEMBER_LEFT             = 0x03B1, // W being id
+    CPMSG_PARTY_NEW_MEMBER              = 0x03B0, // S name, S inviter
+    CPMSG_PARTY_MEMBER_LEFT             = 0x03B1, // D character id
 
     // Chat
     CPMSG_ERROR                    = 0x0401, // B error
-    CPMSG_ANNOUNCEMENT             = 0x0402, // S text
+    CPMSG_ANNOUNCEMENT             = 0x0402, // S text, S sender
     CPMSG_PRIVMSG                  = 0x0403, // S user, S text
     CPMSG_PUBMSG                   = 0x0404, // W channel, S user, S text
     PCMSG_CHAT                     = 0x0410, // S text, W channel
-    PCMSG_ANNOUNCE                 = 0x0411, // S text
     PCMSG_PRIVMSG                  = 0x0412, // S user, S text
     PCMSG_WHO                      = 0x0415, // -
     CPMSG_WHO_RESPONSE             = 0x0416, // { S user }
@@ -239,28 +245,36 @@ enum {
 
     // Inter-server
     GAMSG_REGISTER              = 0x0500, // S address, W port, S password, D items db revision, { W map id }*
-    AGMSG_REGISTER_RESPONSE     = 0x0501, // C item version, C password response
-    AGMSG_ACTIVE_MAP            = 0x0502, // W map id
+    AGMSG_REGISTER_RESPONSE     = 0x0501, // W item version, W password response, { S globalvar_key, S globalvar_value }
+    AGMSG_ACTIVE_MAP            = 0x0502, // W map id, W Number of mapvar_key mapvar_value sent, { S mapvar_key, S mapvar_value }, W Number of map items, { D item Id, W amount, W posX, W posY }
     AGMSG_PLAYER_ENTER          = 0x0510, // B*32 token, D id, S name, serialised character data
     GAMSG_PLAYER_DATA           = 0x0520, // D id, serialised character data
     GAMSG_REDIRECT              = 0x0530, // D id
     AGMSG_REDIRECT_RESPONSE     = 0x0531, // D id, B*32 token, S game address, W game port
     GAMSG_PLAYER_RECONNECT      = 0x0532, // D id, B*32 token
     GAMSG_PLAYER_SYNC           = 0x0533, // serialised sync data
-    GAMSG_SET_QUEST             = 0x0540, // D id, S name, S value
-    GAMSG_GET_QUEST             = 0x0541, // D id, S name
-    AGMSG_GET_QUEST_RESPONSE    = 0x0542, // D id, S name, S value
+    GAMSG_SET_VAR_CHR           = 0x0540, // D id, S name, S value
+    GAMSG_GET_VAR_CHR           = 0x0541, // D id, S name
+    AGMSG_GET_VAR_CHR_RESPONSE  = 0x0542, // D id, S name, S value
+    //reserved GAMSG_SET_VAR_ACC           = 0x0543, // D charid, S name, S value
+    //reserved GAMSG_GET_VAR_ACC           = 0x0544, // D charid, S name
+    //reserved AGMSG_GET_VAR_ACC_RESPONSE  = 0x0545, // D charid, S name, S value
+    GAMSG_SET_VAR_MAP           = 0x0546, // D mapid, S name, S value
+    GAMSG_SET_VAR_WORLD         = 0x0547, // S name, S value
+    AGMSG_SET_VAR_WORLD         = 0x0548, // S name, S value
     GAMSG_BAN_PLAYER            = 0x0550, // D id, W duration
     GAMSG_CHANGE_PLAYER_LEVEL   = 0x0555, // D id, W level
     GAMSG_CHANGE_ACCOUNT_LEVEL  = 0x0556, // D id, W level
-    GAMSG_STATISTICS            = 0x0560, // { W map id, W thing nb, W monster nb, W player nb, { D character id }* }*
+    GAMSG_STATISTICS            = 0x0560, // { W map id, W entity nb, W monster nb, W player nb, { D character id }* }*
     CGMSG_CHANGED_PARTY         = 0x0590, // D character id, D party id
     GCMSG_REQUEST_POST          = 0x05A0, // D character id
     CGMSG_POST_RESPONSE         = 0x05A1, // D receiver id, { S sender name, S letter, W num attachments { W attachment item id, W quantity } }
     GCMSG_STORE_POST            = 0x05A5, // D sender id, S receiver name, S letter, { W attachment item id, W quantity }
     CGMSG_STORE_POST_RESPONSE   = 0x05A6, // D id, B error
     GAMSG_TRANSACTION           = 0x0600, // D character id, D action, S message
-
+    GAMSG_CREATE_ITEM_ON_MAP    = 0x0601, // D map id, D item id, W amount, W pos x, W pos y
+    GAMSG_REMOVE_ITEM_ON_MAP    = 0x0602, // D map id, D item id, W amount, W pos x, W pos y
+    GAMSG_ANNOUNCE              = 0x0603, // S text, W senderid, S sendername
 
     XXMSG_DEBUG_FLAG            = 0x8000, // Message in debug mode
     XXMSG_INVALID               = 0x7FFF
@@ -280,7 +294,8 @@ enum {
     ERRMSG_SERVER_FULL,                 // the server is overloaded
     ERRMSG_TIME_OUT,                    // data failed to arrive in due time
     ERRMSG_LIMIT_REACHED,               // limit reached
-    ERRMSG_ADMINISTRATIVE_LOGOFF        // kicked by server administrator
+    ERRMSG_ADMINISTRATIVE_LOGOFF,       // kicked by server administrator
+    ERRMSG_ALREADY_MEMBER               // is already member of guild/party
 };
 
 // used in AGMSG_REGISTER_RESPONSE to show state of item db
@@ -300,8 +315,7 @@ enum {
     SYNC_CHARACTER_POINTS    = 0x01,       // D charId, D charPoints, D corrPoints
     SYNC_CHARACTER_ATTRIBUTE = 0x02,       // D charId, D attrId, DF base, DF mod
     SYNC_CHARACTER_SKILL     = 0x03,       // D charId, B skillId, D skill value
-    SYNC_ONLINE_STATUS       = 0x04,       // D charId, B 0x00 = offline, 0x01 = online
-    SYNC_END_OF_BUFFER       = 0xFF        // shows, that the buffer ends here.
+    SYNC_ONLINE_STATUS       = 0x04        // D charId, B 0 = offline, 1 = online
 };
 
 // Login specific return values
@@ -340,8 +354,8 @@ enum AttribmodResponseCode {
     ATTRIBMOD_DENIED
 };
 
-// Object type enumeration
-enum ThingType
+// Entity type enumeration
+enum EntityType
 {
     // A simple item.
     OBJECT_ITEM = 0,
@@ -367,11 +381,6 @@ enum {
     MOVING_POSITION = 1,
     // Payload contains the destination.
     MOVING_DESTINATION = 2
-};
-
-// Email change specific return values
-enum {
-    EMAILCHG_EXISTS_EMAIL = 0x40
 };
 
 // Chat errors return values
@@ -440,21 +449,20 @@ enum BeingDirection
 };
 
 /**
-  * enum for sprites layers.
-  * WARNING: Has to be in sync with the same enum in the Sprite class
-  * of the client!
-  */
-enum SpriteLayer
+ * Beings Genders
+ */
+enum BeingGender
 {
-    SPRITE_BASE = 0,
-    SPRITE_SHOE,
-    SPRITE_BOTTOMCLOTHES,
-    SPRITE_TOPCLOTHES,
-    SPRITE_HAIR,
-    SPRITE_HAT,
-    SPRITE_WEAPON,
-    SPRITE_VECTOREND
+    GENDER_MALE = 0,
+    GENDER_FEMALE,
+    GENDER_UNSPECIFIED
 };
+
+/** The permited range to pick up an item */
+const int PICKUP_RANGE = 32 + 16;
+
+/** The permited range to to talk to a NPC. */
+const int NPC_TALK_RANGE = 32 * 7;
 
 } // namespace Mana
 
