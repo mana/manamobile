mana/manamobile

commit f52cd9c82553e6fa43f5446e9558b619ce300d99

Separated map loading from map rendering
Map loading is now done by the MapResource that is managed by the
resource manager. The MapItem now takes a MapResource for rendering.

This allows maps to stay in memory for a bit after leaving them, as well
as allowing access to the map for doing collision handling.

src/mana/resource/mapresource.cpp
@@ -1,0 +1,172 @@
+/*
+ * Mana QML plugin
+ * Copyright (C) 2010-2013  Thorbjørn Lindeijer
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "mapresource.h"
+
+#include "mana/resourcemanager.h"
+
+#include "mana/resource/imageresource.h"
+
+#include "tiled/map.h"
+#include "tiled/mapreader.h"
+#include "tiled/tilelayer.h"
+#include "tiled/tileset.h"
+
+#include <QDebug>
+#include <QNetworkReply>
+
+namespace Mana {
+
+MapResource::MapResource(const QUrl &url,
+                         const QString &path,
+                         QObject *parent)
+    : Resource(url, parent)
+    , mMap(0)
+    , mCollisionLayer(0)
+{
+    ResourceManager *resourceManager = ResourceManager::instance();
+    QNetworkReply *reply = resourceManager->requestFile(url);
+    connect(reply, SIGNAL(finished()), this, SLOT(mapFinished()));
+    setStatus(Loading);
+}
+
+void MapResource::mapFinished()
+{
+    QNetworkReply *reply = static_cast<QNetworkReply*>(sender());
+    reply->deleteLater();
+
+    if (reply->error() != QNetworkReply::NoError) {
+        qDebug() << "Failed to download map:" << reply->url() << "\n"
+                 << reply->errorString();
+        setStatus(Error);
+        return;
+    }
+
+    Tiled::MapReader reader;
+    reader.setLazy(true); // Don't have it load external resources immediately
+
+    mMap = reader.readMap(reply, mPath);
+    if (!mMap) {
+        qDebug() << "Failed to load map:" << reply->url() << "\n"
+                 << reader.errorString();
+        setStatus(Error);
+        return;
+    }
+
+    foreach (Tiled::Layer *layer, mMap->layers()) {
+        if (Tiled::TileLayer *tl = dynamic_cast<Tiled::TileLayer*>(layer)) {
+            if (tl->name().compare(QLatin1String("collision"), Qt::CaseInsensitive) == 0) {
+                mCollisionLayer = tl;
+                continue;
+            }
+        }
+    }
+
+    // Request the external tilesets that were not loaded yet
+    foreach (Tiled::Tileset *tileset, mMap->tilesets()) {
+        if (!tileset->fileName().isEmpty()) {
+            ResourceManager *rm = ResourceManager::instance();
+            QNetworkReply *reply = rm->requestFile(tileset->fileName());
+            mPendingResources.append(reply);
+            connect(reply, SIGNAL(finished()), this, SLOT(tilesetFinished()));
+        } else if (!tileset->imageSource().isEmpty()) {
+            requestTilesetImage(tileset);
+        }
+    }
+}
+
+void MapResource::tilesetFinished()
+{
+    QNetworkReply *reply = finishReply();
+    if (!mMap) // Ignore request when we already changed map again
+        return;
+
+    const QNetworkRequest request = reply->request();
+    const QString tilesetFilePath =
+            request.attribute(ResourceManager::requestedFileAttribute()).toString();
+    Q_ASSERT(!tilesetFilePath.isEmpty());
+
+    const int lastSlashPos = tilesetFilePath.lastIndexOf(QLatin1Char('/'));
+    const QString tilesetPath = tilesetFilePath.left(lastSlashPos);
+
+    Tiled::MapReader reader;
+    reader.setLazy(true); // Don't have it load the tileset images immediately
+
+    Tiled::Tileset *tileset = reader.readTileset(reply, tilesetPath);
+    if (!tileset) {
+        qDebug() << "Error reading tileset:" << tilesetFilePath << "\n"
+                 << reader.errorString();
+    } else {
+        if (!tileset->imageSource().isEmpty())
+            requestTilesetImage(tileset);
+
+        foreach (Tiled::Tileset *ts, mMap->tilesets()) {
+            if (ts->fileName() == tilesetFilePath) {
+                mMap->replaceTileset(ts, tileset);
+                delete ts;
+                break;
+            }
+        }
+    }
+
+    checkReady();
+}
+
+void MapResource::imageStatusChanged()
+{
+    ImageResource *imgRes = static_cast<ImageResource*>(sender());
+    mPendingImageResources.remove(imgRes);
+
+    if (imgRes->status() != Resource::Ready)
+        qWarning() << "Error loading tileset image" << imgRes->url();
+
+    checkReady();
+}
+
+QNetworkReply *MapResource::finishReply()
+{
+    QNetworkReply *reply = static_cast<QNetworkReply*>(sender());
+    reply->deleteLater();
+    mPendingResources.removeOne(reply);
+    return reply;
+}
+
+void MapResource::checkReady()
+{
+    if (status() == Loading) {
+        if (mPendingResources.isEmpty() && mPendingImageResources.isEmpty())
+            setStatus(Ready);
+    }
+}
+
+void MapResource::requestTilesetImage(Tiled::Tileset *tileset)
+{
+    Q_ASSERT(!tileset->imageSource().isEmpty());
+
+    ResourceManager *rm = ResourceManager::instance();
+    ImageResource *imgRes = rm->requestImage(tileset->imageSource());
+    mImageResources.insert(tileset, imgRes);
+
+    if (imgRes->status() == Resource::Loading) {
+        mPendingImageResources.insert(imgRes);
+        connect(imgRes, SIGNAL(statusChanged(Resource::Status)),
+                this, SLOT(imageStatusChanged()));
+    }
+}
+
+} // namespace Mana

example/qml/main/GamePage.qml
@@ -49,7 +49,7 @@ Item {
         anchors.top: parent.top;
         anchors.left: parent.left;
         anchors.margins: 2;
-        text: "Current map: " + gameClient.currentMap;
+        text: "Current map: " + gameClient.currentMapName;
     }
 
     Keys.onReturnPressed: chatBar.open();

example/qml/main/Viewport.qml
@@ -67,7 +67,7 @@ Item {
 
     TileMap {
         id: map;
-        source: gameClient.currentMap;
+        mapResource: gameClient.currentMapResource;
 
         visibleArea: Qt.rect(-map.x,
                              -map.y,

manamobile.qbs
@@ -92,6 +92,8 @@ Project {
                 "mana/resourcelistmodel.h",
                 "mana/resourcemanager.cpp",
                 "mana/resourcemanager.h",
+                "mana/resource/mapresource.cpp",
+                "mana/resource/mapresource.h",
                 "mana/resource/monsterdb.cpp",
                 "mana/resource/monsterdb.h",
                 "mana/resource/npcdb.cpp",

src/mana/gameclient.cpp
@@ -29,10 +29,10 @@
 #include "monster.h"
 #include "npc.h"
 #include "protocol.h"
+#include "resourcemanager.h"
 
 #include "resource/abilitydb.h"
-#include "resource/hairdb.h"
-#include "resource/spritedef.h"
+#include "resource/mapresource.h"
 
 #include <safeassert.h>
 
@@ -246,6 +246,7 @@ void GameClient::update(qreal deltaTime)
 
             direction.normalize();
             direction *= walkDistance;
+            // TODO: Check whether we can walk there
             QPointF newPos(pos.x() + direction.x(), pos.y() + direction.y());
             being->lookAt(newPos);
             being->setPosition(newPos);
@@ -321,16 +322,14 @@ void GameClient::handleAuthenticationResponse(MessageIn &message)
 
 void GameClient::handlePlayerMapChanged(MessageIn &message)
 {
-    const QString name = message.readString();
+    mCurrentMap = message.readString();
     mPlayerStartX = message.readInt16();
     mPlayerStartY = message.readInt16();
 
-    mCurrentMap = QLatin1String("maps/");
-    mCurrentMap += name;
+    if (mMapResource)
+        mMapResource->decRef();
 
-    const QLatin1String mapExtension(".tmx");
-    if (!mCurrentMap.endsWith(mapExtension))
-        mCurrentMap += mapExtension;
+    mMapResource = ResourceManager::instance()->requestMap(mCurrentMap);
 
     // Reset the player being before it gets deleted
     if (mPlayerCharacter) {

src/mana/gameclient.h
@@ -33,6 +33,7 @@ class Being;
 class BeingListModel;
 class Character;
 class LogicDriver;
+class MapResource;
 class NPC;
 
 /**
@@ -46,7 +47,8 @@ class GameClient : public ENetClient
     Q_OBJECT
 
     Q_PROPERTY(bool authenticated READ authenticated NOTIFY authenticatedChanged)
-    Q_PROPERTY(QString currentMap READ currentMap NOTIFY mapChanged)
+    Q_PROPERTY(QString currentMapName READ currentMapName NOTIFY mapChanged)
+    Q_PROPERTY(Mana::MapResource *currentMapResource READ currentMapResource NOTIFY mapChanged)
     Q_PROPERTY(int playerStartX READ playerStartX NOTIFY mapChanged)
     Q_PROPERTY(int playerStartY READ playerStartY NOTIFY mapChanged)
     Q_PROPERTY(Mana::BeingListModel *beingListModel READ beingListModel CONSTANT)
@@ -77,7 +79,8 @@ public:
 
     bool authenticated() const { return mAuthenticated; }
 
-    QString currentMap() const { return mCurrentMap; }
+    QString currentMapName() const { return mCurrentMap; }
+    MapResource *currentMapResource() const { return mMapResource; }
     int playerStartX() const { return mPlayerStartX; }
     int playerStartY() const { return mPlayerStartY; }
 
@@ -159,6 +162,7 @@ private:
 
     bool mAuthenticated;
     QString mCurrentMap;
+    MapResource *mMapResource;
     int mPlayerStartX;
     int mPlayerStartY;
 

src/mana/manaplugin.cpp
@@ -27,6 +27,8 @@
 #include "enetclient.h"
 #include "gameclient.h"
 #include "mapitem.h"
+#include "resourcelistmodel.h"
+#include "resourcemanager.h"
 #include "settings.h"
 #include "spriteitem.h"
 #include "spritelistmodel.h"
@@ -35,8 +37,7 @@
 #include "resource/attributedb.h"
 #include "resource/hairdb.h"
 #include "resource/itemdb.h"
-#include "resourcelistmodel.h"
-#include "resourcemanager.h"
+#include "resource/mapresource.h"
 #include "resource/monsterdb.h"
 #include "resource/npcdb.h"
 #include "resource/racedb.h"
@@ -106,6 +107,7 @@ void ManaPlugin::registerTypes(const char *uri)
     qmlRegisterType<Mana::ResourceManager>();
     qmlRegisterUncreatableType<Mana::Resource>(uri, 1, 0, "Resource",
                                                "Base of all resources");
+    qmlRegisterType<Mana::MapResource>();
     qmlRegisterType<Mana::ResourceListModel>();
 
     qmlRegisterType<Mana::AbilityDB>();

src/mana/mapitem.cpp
@@ -25,38 +25,41 @@
 #include "tiled/map.h"
 #include "tiled/orthogonalrenderer.h"
 #include "tiled/tilelayer.h"
-#include "tiled/tileset.h"
-#include "tiled/mapreader.h"
 
-#include "mana/resource/imageresource.h"
+#include "mana/resource/mapresource.h"
 
-#include <QFileInfo>
-#include <QImageReader>
-#include <QNetworkReply>
-
-#include <QDebug>
-
-using namespace Tiled;
 using namespace Mana;
 
 MapItem::MapItem(QQuickItem *parent)
     : QQuickItem(parent)
-    , mStatus(Null)
-    , mMap(0)
+    , mMapResource(0)
     , mRenderer(0)
 {
 }
 
-void MapItem::setSource(const QString &source)
+void MapItem::setMapResource(MapResource *map)
 {
-    if (mSource == source)
+    if (mMapResource == map)
         return;
 
-    mSource = source;
-    emit sourceChanged(mSource);
+    if (mMapResource)
+        mMapResource->disconnect(this);
 
-    if (isComponentComplete())
-        load();
+    mMapResource = map;
+    emit statusChanged();
+    emit mapChanged();
+
+    if (mMapResource) {
+        connect(mMapResource, SIGNAL(statusChanged(Resource::Status)),
+                SLOT(mapStatusChanged()));
+    }
+
+    refresh();
+}
+
+MapItem::Status MapItem::status() const
+{
+    return mMapResource ? static_cast<Status>(mMapResource->status()) : Null;
 }
 
 void MapItem::setVisibleArea(const QRectF &visibleArea)
@@ -67,7 +70,7 @@ void MapItem::setVisibleArea(const QRectF &visibleArea)
 
 QRectF MapItem::boundingRect() const
 {
-    if (!mMap)
+    if (!mRenderer)
         return QRectF();
 
     return QRectF(QPointF(), mRenderer->mapSize());
@@ -76,108 +79,46 @@ QRectF MapItem::boundingRect() const
 void MapItem::componentComplete()
 {
     QQuickItem::componentComplete();
-    if (!mSource.isEmpty())
-        load();
+    if (mMapResource)
+        refresh();
 }
 
-void MapItem::setStatus(Status status)
+void MapItem::mapStatusChanged()
 {
-    if (mStatus == status)
-        return;
+    if (mMapResource->status() == Resource::Ready)
+        refresh();
 
-    mStatus = status;
     emit statusChanged();
 }
 
-void MapItem::load()
+void MapItem::refresh()
 {
+    if (!isComponentComplete())
+        return;
+
     // Clean up ourselves (maybe wait until the map is available?)
     qDeleteAll(mTileLayerItems);
     mTileLayerItems.clear();
-    delete mMap;
     delete mRenderer;
-    mMap = 0;
     mRenderer = 0;
 
-    // Cancel any previous requests
-    foreach (QNetworkReply *reply, mPendingResources) {
-        disconnect(reply, SIGNAL(finished()));
-        reply->deleteLater();
-    }
-    mPendingResources.clear();
-
-    foreach (ImageResource *imageResource, mPendingImageResources) {
-        disconnect(imageResource, SIGNAL(statusChanged(Resource::Status)),
-                   this, SLOT(imageStatusChanged()));
-    }
-    mPendingImageResources.clear();
-
-    foreach (ImageResource *imageResource, mImageResources)
-        imageResource->decRef();
-    mImageResources.clear();
-
-    // Request the new map
-    QNetworkReply *reply = ResourceManager::instance()->requestFile(mSource);
-    mPendingResources.append(reply);
-    mMapReply = reply;
-
-    connect(reply, SIGNAL(finished()), this, SLOT(mapFinished()));
-
-    setStatus(Loading);
-}
-
-QNetworkReply *MapItem::finishReply()
-{
-    QNetworkReply *reply = static_cast<QNetworkReply*>(sender());
-    reply->deleteLater();
-    mPendingResources.removeOne(reply);
-    return reply;
-}
-
-void MapItem::checkReady()
-{
-    if (mStatus == Loading) {
-        if (mPendingResources.isEmpty() && mPendingImageResources.isEmpty())
-            setStatus(Ready);
-    }
-}
-
-void MapItem::mapFinished()
-{
-    QNetworkReply *reply = finishReply();
-
-    // Ignore late replies if the map has changed meanwhile
-    if (reply != mMapReply)
+    if (!mMapResource || mMapResource->status() != Resource::Ready)
         return;
 
-    if (reply->error() != QNetworkReply::NoError) {
-        qDebug() << "Failed to download map:" << mSource << "\n"
-                 << reply->errorString();
-        return;
-    }
+    const Tiled::Map *map = mMapResource->map();
 
-    Tiled::MapReader reader;
-    reader.setLazy(true); // Don't have it load external resources immediately
-
-    mMap = reader.readMap(reply, QFileInfo(mSource).path());
-    if (!mMap) {
-        qDebug() << "Failed to load map:" << mSource << "\n"
-                 << reader.errorString();
-        return;
-    }
-
-    switch (mMap->orientation()) {
-    case Map::Isometric:
-        mRenderer = new IsometricRenderer(mMap);
+    switch (map->orientation()) {
+    case Tiled::Map::Isometric:
+        mRenderer = new Tiled::IsometricRenderer(map);
         break;
     default:
-        mRenderer = new OrthogonalRenderer(mMap);
+        mRenderer = new Tiled::OrthogonalRenderer(map);
         break;
     }
 
     bool seenFringe = false;
-    foreach (Layer *layer, mMap->layers()) {
-        if (TileLayer *tl = dynamic_cast<TileLayer*>(layer)) {
+    foreach (Tiled::Layer *layer, map->layers()) {
+        if (Tiled::TileLayer *tl = layer->asTileLayer()) {
             if (tl->name().compare(QLatin1String("collision"), Qt::CaseInsensitive) == 0)
                 continue;
 
@@ -192,94 +133,4 @@ void MapItem::mapFinished()
 
     const QSize size = mRenderer->mapSize();
     setImplicitSize(size.width(), size.height());
-
-    // Request the external resources that were not loaded yet
-    foreach (Tileset *tileset, mMap->tilesets()) {
-        if (!tileset->fileName().isEmpty()) {
-            ResourceManager *rm = ResourceManager::instance();
-            QNetworkReply *reply = rm->requestFile(tileset->fileName());
-            mPendingResources.append(reply);
-            connect(reply, SIGNAL(finished()), this, SLOT(tilesetFinished()));
-        }
-        else if (!tileset->imageSource().isEmpty()) {
-            requestTilesetImage(tileset);
-        }
-    }
-
-    checkReady();
-}
-
-void MapItem::tilesetFinished()
-{
-    QNetworkReply *reply = finishReply();
-    if (!mMap) // Ignore request when we already changed map again
-        return;
-
-    const QNetworkRequest request = reply->request();
-    const QString tilesetFilePath =
-            request.attribute(ResourceManager::requestedFileAttribute()).toString();
-    Q_ASSERT(!tilesetFilePath.isEmpty());
-
-    const int lastSlashPos = tilesetFilePath.lastIndexOf(QLatin1Char('/'));
-    const QString tilesetPath = tilesetFilePath.left(lastSlashPos);
-
-    Tiled::MapReader reader;
-    reader.setLazy(true); // Don't have it load the tileset images immediately
-
-    Tiled::Tileset *tileset = reader.readTileset(reply, tilesetPath);
-    if (!tileset) {
-        qDebug() << "Error reading tileset:" << tilesetFilePath << "\n"
-                 << reader.errorString();
-    } else {
-        if (!tileset->imageSource().isEmpty())
-            requestTilesetImage(tileset);
-
-        foreach (Tileset *ts, mMap->tilesets()) {
-            if (ts->fileName() == tilesetFilePath) {
-                mMap->replaceTileset(ts, tileset);
-                delete ts;
-                break;
-            }
-        }
-
-        // The above tileset replacement may change the tile layer draw margins
-        foreach (TileLayerItem *layerItem, mTileLayerItems)
-            layerItem->updateVisibleTiles();
-    }
-
-    checkReady();
-}
-
-void MapItem::imageStatusChanged()
-{
-    ImageResource *imgRes = static_cast<ImageResource*>(sender());
-    mPendingImageResources.remove(imgRes);
-
-    // Trigger a repaint of the tile layer items
-    if (imgRes->status() == Resource::Ready) {
-        foreach (TileLayerItem *layerItem, mTileLayerItems)
-            layerItem->update();
-    } else {
-        qWarning() << "Error loading tileset image" << imgRes->url();
-    }
-
-    checkReady();
-}
-
-void MapItem::requestTilesetImage(Tileset *tileset)
-{
-    Q_ASSERT(!tileset->imageSource().isEmpty());
-
-    ResourceManager *rm = ResourceManager::instance();
-    ImageResource *imgRes = rm->requestImage(tileset->imageSource());
-    mImageResources.insert(tileset, imgRes);
-
-    if (imgRes->status() == Resource::Loading) {
-        mPendingImageResources.insert(imgRes);
-        connect(imgRes, SIGNAL(statusChanged(Resource::Status)),
-                this, SLOT(imageStatusChanged()));
-    } else {
-        foreach (TileLayerItem *layerItem, mTileLayerItems)
-            layerItem->update();
-    }
 }

commit ddfe27d70ff181f5a512fe0e4e49128aa036a80c

Fixed map loading when all external resources are already there
A call to 'checkReady' got lost in commit f52cd9c82553e6fa43f, which
caused the map to never come out of 'Loading' state when all tileset
images referenced by the map were already available.

src/mana/resource/mapresource.cpp
@@ -90,6 +90,8 @@ void MapResource::mapFinished()
             requestTilesetImage(tileset);
         }
     }
+
+    checkReady();
 }
 
 void MapResource::tilesetFinished()